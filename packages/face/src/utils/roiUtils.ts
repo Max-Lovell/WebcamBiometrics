5cd57d79bc167e61641acfb900054619fb9f66ba
Max Lovell <max_lovell@hotmail.co.uk>
2026-01-26 16:07:58 +0000

    Hard code facelandmarker ROI numbers for forehead and cheeks to utils file

---
+++ packages/face/src/utils/roiUtils.ts
@@ -0,0 +1,9 @@
+// MediaPipe Face Mesh Indices for rPPG signal
+export const FACE_ROIS = {
+    // Center forehead
+    forehead: [109, 10, 338, 297, 332, 284, 251, 389, 356],
+    // Left Cheek
+    leftCheek: [330, 347, 446, 266, 425],
+    // Right Cheek
+    rightCheek: [101, 118, 226, 36, 205]
+};
