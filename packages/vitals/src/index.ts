bcd70afd3ba110337c2047ef9e603b2b20f38c75
Max Lovell <max_lovell@hotmail.co.uk>
2026-02-12 16:43:58 +0000

    Vitals index export just the main function

--- packages/vitals/src/index.ts
+++ packages/vitals/src/index.ts
@@ -1,1 +1,5 @@
-export * from './image/roi';
+// export * from './image/roi';
+// export * from './signal/SlidingBuffer';
+// export * from './signal/POS';
+// export * from './signal/SignalProcessor';
+export * from './HeartRateEstimator';
