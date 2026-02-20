e344718582e13a9b00acdc7909b75dca5943d6ae
Max Lovell <max_lovell@hotmail.co.uk>
2026-02-20 16:24:00 +0000

    Add package-wide types file with video frame and point xy types

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+
+
+export interface Point {
+    x: number;
+    y: number;
+}
+
+// Note HTMLVideoElement doesn't work in a worker, but that's up to the user to pass in the correct data
+export type VideoFrameData = VideoFrame | ImageData | ImageBitmap | HTMLVideoElement;
