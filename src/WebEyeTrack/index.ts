fbd5f144c91aada0db8754fedc19a720af57e859
Max Lovell <max_lovell@hotmail.co.uk>
2026-03-04 12:04:27 +0000

    Delete old WebEyeTrack Proxy and Worker

--- src/WebEyeTrack/index.ts
+++ src/WebEyeTrack/index.ts
@@ -1,18 +1,8 @@
 import WebEyeTrack from './WebEyeTrack.ts'
-import WebEyeTrackProxy from './WebEyeTrackProxy.ts'
-import WebcamClient from '../Core/WebcamClient.ts'
-import FaceLandmarkerClient from '../Core/FaceLandmarkerClient.ts'
 import BlazeGaze from "./BlazeGaze.ts"
-import type {BiometricsResult} from "../pipeline/types.ts";
-// TODO: delete
+
 export {
-    WebEyeTrackProxy,
     WebEyeTrack,
-    WebcamClient,
-    FaceLandmarkerClient,
     BlazeGaze
 }
 
-export type {
-    BiometricsResult
-}
