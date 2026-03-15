c9b12fe04084ade18a37dee0d28a27f575172e86
Max Lovell <max_lovell@hotmail.co.uk>
2026-03-15 14:38:45 +0000

    Remove unused faceOriginMetric from WebEyeTrack

--- src/WebEyeTrack/utils/faceOriginMetric.ts
+++
@@ -1,9 +0,0 @@
-import type {FaceLandmarkerResult} from "@mediapipe/tasks-vision";
-
-function faceOrigin(faceLandmarkerResult: FaceLandmarkerResult){
-    const facialTransformation = faceLandmarkerResult.facialTransformationMatrixes[0]
-    // take relationship to facial transformation matrix
-
-    // Why can't you get the metric location of the facial transform
-
-}
