4c418f0dd6bab9d62353a78ca0b0af1144851f33
Max Lovell <max_lovell@hotmail.co.uk>
2026-04-13 10:36:12 +0100

    Draw head axes on top of centre dot and make lines smaller for clarity

--- demo/drawing/drawHeadAxes.ts
+++ demo/drawing/drawHeadAxes.ts
@@ -7,19 +7,30 @@ export function drawHeadAxes(
     ctx: CanvasRenderingContext2D,
     axes: { origin: Point; xAxis: Point; yAxis: Point; zAxis: Point },
 ): void {
+    // Origin dot
+    ctx.save();
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.arc(axes.origin.x, axes.origin.y, 2, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
+
     const drawLine = (to: Point, color: string, label: string) => {
         ctx.save();
         ctx.strokeStyle = color;
-        ctx.lineWidth = 3;
+        ctx.lineWidth = 1;
         ctx.beginPath();
         ctx.moveTo(axes.origin.x, axes.origin.y);
         ctx.lineTo(to.x, to.y);
         ctx.stroke();
 
         ctx.fillStyle = color;
-        ctx.font = 'bold 14px sans-serif';
+        ctx.font = 'bold 10px sans-serif';
         ctx.strokeStyle = 'black';
-        ctx.lineWidth = 3;
+        ctx.lineWidth = 1;
         ctx.strokeText(label, to.x + 4, to.y);
         ctx.fillText(label, to.x + 4, to.y);
         ctx.restore();
@@ -28,15 +39,4 @@ export function drawHeadAxes(
     drawLine(axes.xAxis, 'red', 'X');
     drawLine(axes.yAxis, 'lime', 'Y');
     drawLine(axes.zAxis, 'blue', 'Z');
-
-    // Origin dot
-    ctx.save();
-    ctx.fillStyle = 'white';
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    ctx.arc(axes.origin.x, axes.origin.y, 5, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.stroke();
-    ctx.restore();
 }
